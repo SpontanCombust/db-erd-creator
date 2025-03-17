728889bf5539035bf218f807ec84b7e7f6083dc0
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-03-17 15:22:43 +0100

    Project scaffold

---
+++ README.md
@@ -0,0 +1,2 @@
+# db-erd-creator
+
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+
+<template>
+  <header>
+    Hello world!
+  </header>
+
+  <main>
+    
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
