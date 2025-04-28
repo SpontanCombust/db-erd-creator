f3e72b39f60399c877ca33b84f95141b0945f08d
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-04-28 14:01:31 +0200

    fix service injection

--- src/composables/useService.ts
+++ src/composables/useService.ts
@@ -4,11 +4,12 @@ import { inject, provide } from "vue";
 type ServiceClass<S> = { new (...args: any[]) : S };
 
 export function provideService<S>(serviceClass: ServiceClass<S>, serviceInstance?: S) {
-    const serviceName = serviceClass.constructor.name;
+    const serviceName = serviceClass.name;
+    console.log("Registered " + serviceClass.name + " service!");
     provide(serviceName, serviceInstance ?? new serviceClass())
 }
 
 export function useService<S extends Object>(serviceClass: ServiceClass<S>) : S {
-    const serviceName = serviceClass.constructor.name;
+    const serviceName = serviceClass.name;
     return inject(serviceName, new serviceClass());
 }
