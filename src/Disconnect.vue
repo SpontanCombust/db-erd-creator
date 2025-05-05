24ba5bc6ec41cfd4dbda373fef6732aa76db8a7f
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-05-05 15:56:15 +0200

    fix connection data not persisting with reloads

--- src/Disconnect.vue
+++ src/Disconnect.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { disconnectFromDb } from './api/tauri';
+import { useDbConnectionStore } from './stores/DbConnectionStore';
 
+
+const { releaseConnection } = useDbConnectionStore();
 
 onMounted(async () => {
   disconnectFromDb();
+  releaseConnection();
 
   setTimeout(() => {
     window.location.hash = '#/';
--- src/stores/DbConnectionStore.ts
+++ src/stores/DbConnectionStore.ts
@@ -4,21 +4,55 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 
-export const useDbConnectionStore = defineStore('DbConnectionStore', () => {
-    const driver = ref('');
-    const dbKind = ref(SupportedDbKind.SQLite);
-    const dbName = ref('');
+const DRIVER_STORAGE_KEY = 'dbConn/driver';
+const DB_KIND_STORAGE_KEY = 'dbConn/dbKind';
+const DB_NAME_STORAGE_KEY = 'dbConn/dbName';
+const DB_CONN_STRING_STORAGE_KEY = 'dbConn/connString';
 
-    function storeConnection(driv: string, kind: SupportedDbKind, name: string) {
+export const useDbConnectionStore = defineStore('DbConnectionStore', () => {
+    const sessionDriver = sessionStorage.getItem(DRIVER_STORAGE_KEY);
+    const sessionDbKind = sessionStorage.getItem(DB_KIND_STORAGE_KEY);
+    const sessionDbName = sessionStorage.getItem(DB_NAME_STORAGE_KEY);
+    const sessionDbConnString = sessionStorage.getItem(DB_CONN_STRING_STORAGE_KEY);
+
+    const connected = ref(sessionDbConnString != null);
+    const driver = ref(sessionDriver ?? '');
+    const dbKind = ref(sessionDbKind ? Number.parseInt(sessionDbKind) : SupportedDbKind.SQLite);
+    const dbName = ref(sessionDbName ?? '');
+    const connectionString = ref(sessionDbConnString ?? '');
+
+    function storeConnection(driv: string, kind: SupportedDbKind, name: string, connString: string) {
+        connected.value = true;
         driver.value = driv;
         dbKind.value = kind;
         dbName.value = name;
+        connectionString.value = connString;
+
+        sessionStorage.setItem(DRIVER_STORAGE_KEY, driv);
+        sessionStorage.setItem(DB_KIND_STORAGE_KEY, kind.toString());
+        sessionStorage.setItem(DB_NAME_STORAGE_KEY, name);
+        sessionStorage.setItem(DB_CONN_STRING_STORAGE_KEY, connString);
+    }
+
+    function releaseConnection() {
+        connected.value = false;
+        driver.value = '';
+        dbKind.value = 0;
+        dbName.value = '';
+
+        sessionStorage.removeItem(DRIVER_STORAGE_KEY);
+        sessionStorage.removeItem(DB_KIND_STORAGE_KEY);
+        sessionStorage.removeItem(DB_NAME_STORAGE_KEY);
+        sessionStorage.removeItem(DB_CONN_STRING_STORAGE_KEY);
     }
 
     return {
+        connected,
         driver,
         dbKind,
         dbName,
-        storeConnection
+        connectionString,
+        storeConnection,
+        releaseConnection
     }
 });
