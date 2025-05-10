255b4e4fae30250afa5f69463ecdc983908e11c4
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2025-05-10 21:07:44 +0200

    fix: clear stores after disconnect

--- src/Disconnect.vue
+++ src/Disconnect.vue
@@ -2,13 +2,22 @@
 import { onMounted } from 'vue';
 import { disconnectFromDb } from './api/tauri';
 import { useDbConnectionStore } from './stores/DbConnectionStore';
+import { useDbTableStore } from './stores/DbTableStore';
+import { useDbTableColumnStore } from './stores/DbTableColumnStore';
+import { useDbTableRelationStore } from './stores/DbTableRelationStore';
 
 
 const { releaseConnection } = useDbConnectionStore();
+const { clearTables } = useDbTableStore();
+const { clearColumns } = useDbTableColumnStore();
+const { clearRelations } = useDbTableRelationStore();
 
 onMounted(async () => {
   disconnectFromDb();
   releaseConnection();
+  clearTables();
+  clearColumns();
+  clearRelations();
 
   setTimeout(() => {
     window.location.hash = '#/';
