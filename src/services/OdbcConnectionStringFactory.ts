57742f191744f9b34aa789ca172328ac18bf4514
SpontanCombust <cedro_p@o2.pl>
2025-06-27 22:10:29 +0200

    removed integrated authentication option from MSSQl connection string

--- src/services/OdbcConnectionStringFactory.ts
+++ src/services/OdbcConnectionStringFactory.ts
@@ -12,6 +12,6 @@ export default class OdbcConnectionStringFactory {
     }
 
     sqlserver(driver: string, server: string, port: number, database: string, uid: string, pwd: string) : string {
-        return `Driver={${driver}};Server=${server};Port=${port};Database=${database};UID=${uid};PWD=${pwd}; Trusted_Connection=yes;`;
+        return `Driver={${driver}};Server=${server};Port=${port};Database=${database};UID=${uid};PWD=${pwd};`/*Trusted_Connection=yes;*/;
     }
 }
